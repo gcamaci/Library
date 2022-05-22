bc8bd2d5c01600d23ffab0fc645c08efefbc8803
gcamaci <77690094+gcamaci@users.noreply.github.com>
2022-05-22 15:20:17 -0500

    finished input, new book objects now being stored in array

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+
+//modal DOM
 const closeBtn = document.getElementById('close-modal');
-const modal = document.querySelector('.modal');
+const modal = document.querySelector('.hide-modal');
 const addBook = document.getElementById('modal-button');
 
 
@@ -7,26 +9,52 @@ const addBook = document.getElementById('modal-button');
 
 let myLibrary = [];
 
-
-function Book(title,author,pages,read){
+//constructor
+function Book(title,author,pages){
     this.title = title
     this.author = author
     this.pages = pages
-    this.read = read
+}
+function addBookToLibrary(){
+    //get the input values from user
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    //const read = document.getElementById("checkbox").value;
+
+    const newBook = new Book(title,author,pages);
+    myLibrary.push(newBook);
 
 }
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// we can probably do this an easier way in one function? 
 addBook.addEventListener('click', () => {
-    modal.style.display = "flex";
+   modal.classList.remove('hide-modal');
+   modal.classList.add('modal');
 
 });
 
 
 closeBtn.addEventListener('click', () => {
-    modal.style.display = "none";
+    modal.classList.remove('modal')
+    modal.classList.add("hide-modal");
 });
 
 
