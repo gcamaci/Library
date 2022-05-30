1435e04cb010cf2a24014f9aea096a96866e5861
gcamaci <77690094+gcamaci@users.noreply.github.com>
2022-05-29 20:06:52 -0500

    styling

--- script.js
+++ script.js
@@ -3,12 +3,11 @@
 const closeBtn = document.getElementById('close-modal');
 const modal = document.querySelector('.hide-modal');
 const addBook = document.getElementById('modal-button');
-
+const createBook = document.getElementById('create-btn')
 const shelf = document.querySelector('main');
-
+const form = document.getElementById('book-form')
 
 let myLibrary = [];
-
 
 //Placeholder books
 const book1 = new Book('Giuseppe',"Camaci",299,false);
@@ -25,39 +24,46 @@ function Book(title,author,pages,read){
     this.author = author
     this.pages = pages
     this.read = read
-    
+
 }
 
-Book.prototype.ifRead = (speesh) => {
-    if(myLibrary[speesh].read === false){
-        console.log('im false motha fucka')
+Book.prototype.ifRead = function (){
+    if(this.read === false){
+        this.read = true;
+        console.log(this.read)
+        
     }else{
-        console.log('im true bitch')
+        this.read = false
+        console.log(this.read)
     }
-};
+}
 
 function addBookToLibrary(){
     //get the input values from user
-    const title = document.getElementById("title").value;
-    const author = document.getElementById("author").value;
-    const pages = document.getElementById("pages").value;
-    const reads = document.getElementById("read").value;
-    //console.log(read)
-    //create book and push to libray array
-    const newBook = new Book(title,author,pages,reads);
-    myLibrary.push(newBook);
-    shelf.textContent = '';
-    displayLibrary()
+    const title = document.getElementById("title");
+    const author = document.getElementById("author");
+    const pages = document.getElementById("pages");
+    const reads = document.getElementById("read-check").checked;
     
+    if(title.value.length > 0 && author.value.length > 0 && pages.value > 0){
+        const newBook = new Book(title.value,author.value,pages.value,reads.value);
+        myLibrary.push(newBook);
+        shelf.textContent = '';
+        title.value = '';
+        author.value = '';
+        pages.value = '';
+        console.log(reads)
+        displayLibrary()
+    }  
 }
 /* 
 1) Loop through each index of Mylibrary and create DOM elements per book
 2) Assign each DOM element a data attribute = its index in array
 3) Create toggle for read/unread.
 */
+
 function displayLibrary(){
     myLibrary.forEach((Book,index) => {
-        
     
         const card = document.createElement('div');
         card.dataset.deleteBook = index;
@@ -85,7 +91,6 @@ function displayLibrary(){
         const btnWrapper = document.createElement('div');
         btnWrapper.classList.add('btn-wrapper');
         card.appendChild(btnWrapper);
-
         //create closeout button DOM
         const deleteBtn = document.createElement('button');
         deleteBtn.type = "button";
@@ -97,19 +102,28 @@ function displayLibrary(){
         // read button
         const readBtn = document.createElement('button')
         readBtn.classList.add('btn')
+        readBtn.classList.add('unread')
         readBtn.dataset.readNum = index;
         readBtn.type = "button";
         readBtn.textContent = "read";
+
+        //switch to ternary
+        
+        if(Book.read === true){
+            readBtn.style.border = "2px solid green"
+        }else{
+            readBtn.style.border = "2px solid red"
+        }
+        
         btnWrapper.appendChild(readBtn);
-
+       
         readBtn.addEventListener('click', () => {
-            const readBook = parseInt(readBtn.dataset.readNum)
-            Book.ifRead(readBook);
-            //Book.ifRead()
-
-
+            let bookIndex = myLibrary[parseInt(readBtn.dataset.readNum)]
+            bookIndex.ifRead()
+            shelf.innerHTML = ''
+            displayLibrary()
+ 
         });
-
         //event listeners 
         deleteBtn.addEventListener('click',() => {
             let deleteMe = parseInt(deleteBtn.dataset.deleteBook);
@@ -123,10 +137,13 @@ function deleteBook(bookNum){
     myLibrary.splice(bookNum,1)
     shelf.textContent = '';
     displayLibrary()
-    console.log(myLibrary)
+    
 
 }
+createBook.addEventListener('click', ()=>{
+    addBookToLibrary()
 
+});
 // we can probably do this an easier way in one function? 
 
 addBook.addEventListener('click', () => {
@@ -140,5 +157,7 @@ closeBtn.addEventListener('click', () => {
     modal.classList.add("hide-modal");
 });
 
-
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+});
 displayLibrary();
